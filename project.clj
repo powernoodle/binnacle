ec6e49c44f7ec20c4cb48f19a2b305c60ce8c189
Brendon Walsh <brendonwalsh@niamu.com>
2016-01-27 15:53:57 -0500

    Bump versions of Clojure(Script)

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.powernoodle/binnacle "0.3.0"
+(defproject com.powernoodle/binnacle "0.3.1"
   :description "Binnacle provides images and fonts as data"
   :url "http://github.com/powernoodle/binnacle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.145"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.7.228"]
                  [org.clojure/data.codec "0.1.0"]]
   :source-paths ["src"]
   :main binnacle.core
